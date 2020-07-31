847d00544156f06745a1ebc3cf174d95b1d88e93
Santiago R <santiagorisso5@gmail.com>
2020-07-31 15:24:56 -0300

    botones ready

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import Button from '@material-ui/core/Button';
 import './styles.css';
 
 var textos=[1,2,3];
-
-class TextoPrincipal extends React.Component{
-  renderBotones(i){
-    return<botones value={i}/>;
-  }
-}
-
-class Botones extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={
-      value: null,
-    };
-  }
-
-  render(){
-    return(
-      <Button 
-      className="boton"
-      variant="contained"
-      color="secondary"
-      >
-        {this.state.value}
-      </Button>
-    );
-  }
-}
-
 var i=0;
 
+function Botones(props) {
+  return(
+  <div className="botones">
+    <div className="Volver">
+      <Button color="secondary" onClick={props.hiceclick}>
+        Atras 
+      </Button>
+    </div>
+    <div className="Siguiente">
+      <Button color="primary" onClick={props.hiceclickAdelante}>
+        Adelante
+      </Button>
+    </div>
+  </div>
+  )
+
+}
+
 function App() {
+  var [indice, setearindice]=useState(0);
+  var clickadelante=() => {
+    if(indice<=1){
+      setearindice(indice+1)  
+    }else{
+      setearindice(indice=0)  
+    }
+    /*setearindice(indice+1)*/
+  };
+  var clickatras=() => {
+    if(indice!=0){
+      setearindice(indice-1)
+    }else{
+      setearindice(indice+2)
+    }
+  };
   return (<>
 <header>
     <h1> Falling in the real-time trap </h1>
@@ -44,21 +50,10 @@ function App() {
 <body>
     <div className="MainText">
         <p>
-          {textos[i]}
+          {textos[indice]}
         </p>
     </div>
-    <div className="botones">
-      <div className="Volver">
-        <Botones color="secondary" onClick={}>
-          Atras 
-        </Botones>
-      </div>
-      <div className="Siguiente">
-        <Botones color="primary">
-          Adelante
-        </Botones>
-      </div>
-    </div>
+    <Botones hiceclick={clickatras} hiceclickAdelante={clickadelante}></Botones>
 </body>
   </>);
  
