a1eb7ecbede23f42b716757a05702449a92df42b
Santiago R <santiagorisso5@gmail.com>
2020-09-02 15:41:03 -0300

    no se que hacer

--- src/App.js
+++ src/App.js
@@ -2,10 +2,76 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import Button from '@material-ui/core/Button';
 import './styles.css';
-import { hexToRgb } from '@material-ui/core';
+import { hexToRgb, FormGroup } from '@material-ui/core';
 import Axe from './axe.jpg';
 import Winter from './ww.png';
 import Antimage from './am.png';
+import { withStyles } from '@material-ui/core/styles';
+import { purple } from '@material-ui/core/colors';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+const AntSwitch = withStyles((theme) => ({
+  root: {
+    width: 28,
+    height: 16,
+    padding: 0,
+    display: 'flex'
+  },
+  switchBase: {
+    padding: 2,
+    color: theme.palette.grey[500],
+    '&$checked': {
+      transform: 'translateX(12px)',
+      color: theme.palette.common.white,
+      '& + $track': {
+        opacity: 1,
+        backgroundColor: theme.palette.primary.main,
+        borderColor: theme.palette.primary.main,
+      },
+    },
+  },
+  thumb: {
+    width: 12,
+    height: 12,
+    boxShadow: 'none',
+  },
+  track: {
+    border: `1px solid ${theme.palette.grey[500]}`,
+    borderRadius: 16 / 2,
+    opacity: 1,
+    backgroundColor: theme.palette.common.white,
+  },
+  checked: {},
+}))(Switch);
+
+function CustomizedSwitches() {
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: true,
+    checkedC: true,
+  });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  return (
+    <div class='esen'>
+    <Typography component="div">
+        <Grid component="label" container alignItems="center" spacing={1}>
+          <Grid item>EN</Grid>
+          <Grid item>
+            <AntSwitch checked={state.checkedC} onChange={handleChange} name="checkedC" />
+          </Grid>
+          <Grid item>ES</Grid>
+        </Grid>
+      </Typography>
+      </div>
+  );
+}
 
 function Strength() {
 
@@ -55,6 +121,9 @@ function Botones(props) {
 }
 
 function App() {
+  var espanol="cayendo";
+  var ingles="falling";
+  const titulo=[espanol,ingles];
   const heroes=[<Strength></Strength>,<Intelligence></Intelligence>,<Agility></Agility>];
   var [indice, setearindice]=useState(0);
   var clickadelante=() => {
@@ -86,11 +155,16 @@ function App() {
 
   return (<>
 <header>
-    <h1> Falling in the real-time trap </h1>
+  <div className='titles'>
+  {titulo=[0]}
+  </div>
     <h2> The behind the scenes in the streaming data world. </h2>
 </header>
 
 <body>
+    <div className='enes'>
+      <CustomizedSwitches></CustomizedSwitches>
+    </div>
     <div className="MainText">
       {heroes[indice]}
     </div>
