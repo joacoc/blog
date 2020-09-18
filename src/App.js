92086743956d0a88a5f7da9b3734dc30adbe86de
Santiago R <santiagorisso5@gmail.com>
2020-09-18 03:20:20 -0300

    pasar parametros ?

--- src/App.js
+++ src/App.js
@@ -12,65 +12,26 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+export default App;
 
-const AntSwitch = withStyles((theme) => ({
-  root: {
-    width: 28,
-    height: 16,
-    padding: 0,
-    display: 'flex'
-  },
-  switchBase: {
-    padding: 2,
-    color: theme.palette.grey[500],
-    '&$checked': {
-      transform: 'translateX(12px)',
-      color: theme.palette.common.white,
-      '& + $track': {
-        opacity: 1,
-        backgroundColor: theme.palette.primary.main,
-        borderColor: theme.palette.primary.main,
-      },
-    },
-  },
-  thumb: {
-    width: 12,
-    height: 12,
-    boxShadow: 'none',
-  },
-  track: {
-    border: `1px solid ${theme.palette.grey[500]}`,
-    borderRadius: 16 / 2,
-    opacity: 1,
-    backgroundColor: theme.palette.common.white,
-  },
-  checked: {},
-}))(Switch);
-
-function CustomizedSwitches() {
+function SwitchLabels(props) {
   const [state, setState] = React.useState({
     checkedA: true,
-    checkedB: true,
-    checkedC: true,
   });
+  var estado;
 
   const handleChange = (event) => {
     setState({ ...state, [event.target.name]: event.target.checked });
   };
 
   return (
-    <div class='esen'>
-    <Typography component="div">
-        <Grid component="label" container alignItems="center" spacing={1}>
-          <Grid item>EN</Grid>
-          <Grid item>
-            <AntSwitch checked={state.checkedC} onChange={handleChange} name="checkedC" />
-          </Grid>
-          <Grid item>ES</Grid>
-        </Grid>
-      </Typography>
-      </div>
-  );
+    <FormGroup row>
+      <FormControlLabel
+        control={<Switch checked={state.checkedA} onChange={handleChange} name="checkedA" />}
+        label="Secondary"
+      />
+    </FormGroup>
+  )
 }
 
 function Strength() {
@@ -121,9 +82,8 @@ function Botones(props) {
 }
 
 function App() {
-  var espanol="cayendo";
-  var ingles="falling";
-  const titulo=[espanol,ingles];
+  var checktitles=0;
+  const titulos=['cayendo','falling'];
   const heroes=[<Strength></Strength>,<Intelligence></Intelligence>,<Agility></Agility>];
   var [indice, setearindice]=useState(0);
   var clickadelante=() => {
@@ -140,6 +100,15 @@ function App() {
       setearindice(indice+2)
     }
   };
+
+  var translate=(props) => {
+    if(SwitchLabels.handle){
+      checktitles=0;
+    }else{
+      checktitles=1;
+    }
+  }
+  
   // Codigo de antes:
   // Si vas a usar esta funcion como componente acordate que empiece con mayuscula ej. Imagenamostrar
   // var imagenamostrar=() => {
@@ -155,23 +124,18 @@ function App() {
 
   return (<>
 <header>
-  <div className='titles'>
-  {titulo=[0]}
+  <div className="titulo">
+    <SwitchLabels onChange={}></SwitchLabels>
+    <h1> {titulos[checktitles]} </h1>
   </div>
     <h2> The behind the scenes in the streaming data world. </h2>
 </header>
 
 <body>
-    <div className='enes'>
-      <CustomizedSwitches></CustomizedSwitches>
-    </div>
     <div className="MainText">
       {heroes[indice]}
     </div>
     <Botones indice={indice} hiceclick={clickatras} hiceclickAdelante={clickadelante}></Botones>
 </body>
-  </>);
- 
+  </>)
 }
-
-export default App;
