9c2af36d65378030695e531700bdee49a15d27d8
Joaquin Colacci <joaquincolacci@gmail.com>
2020-12-07 16:18:28 +0000

    Feat: Sensation Two Thousasnd

--- src/components/Pages/SensationTwoThousand/index.js
+++ src/components/Pages/SensationTwoThousand/index.js
@@ -6,14 +6,16 @@ export default function SensationTwoThousand() {
     const content = (
         <>
             <p>
-                <b>Have you ever had the sensation that 2000's were great and crazy years? </b><br/>
-                Well. I have it. And I call them the "2000's Sensation" <br/>
-                I ussualy have this feeling of our present. That everything is alright <br/>
-                That we are living in a very revolutionary and crazy moment <br/>
-                One that will be remembered in 20 years as the 2020's Sensation. <br/>
-                There is also a song from Rels B called "Baladita 2000" <br/>
-                Twenty years far seems to be a moment to be remembered with happines <br/>
-                And maybe. Because of our childhood. 
+                <img className="m-auto" src="twothousand.png" alt="twothousand"></img>
+                <b>Have you ever had the sensation that 2000's were great and crazy years? </b><br/><br/>
+                Well. I have it. I was a child at the time. And I call them the "2000's Sensation" <br/>
+                I usually have this feeling of our present. Also in special occasions of my life. 
+                That feeling that everything is alright.  <br/>
+                That we are living in a very revolutionary and crazy moment. <br/>
+                One that some child will remember in 20 years as the 2020's Sensation. <br/>
+                There is also a song from Rels B called "Baladita 2000" that makes you feel at those times. <br/>
+                Twenty years far seems to be a moment remembered with so much happiness, joy, and craziness.<br/>
+                And maybe. Because of childhood. We have hopefulness and more.
             </p>
         </>
     )
--- src/components/Pages/index.js
+++ src/components/Pages/index.js
@@ -1,8 +1,13 @@
 import FallingInTheRealTimeTrap from "./FallingInTheRealTimeTrap";
 import FPLBTheory from "./FPLBTheory";
 import BenjaminFranklinVirtues from "./BenjaminFranklinVirtues";
+import SensationTwoThousand from "./SensationTwoThousand";
 
 export const routedPages = [
+    {
+        path: "/sensation-two-thousand",
+        page: SensationTwoThousand,
+    },
     {
         path: "/benjamin-franklin-virtues",
         page: BenjaminFranklinVirtues,
