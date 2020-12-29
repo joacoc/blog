690282d895fad97fd71200202701c5a36ab068ba
Joaquin Colacci <joaquincolacci@gmail.com>
2020-12-29 21:02:13 +0000

    [Fix] Introduction to NY Subway

--- src/components/Pages/NYSubway/index.js
+++ src/components/Pages/NYSubway/index.js
@@ -7,12 +7,15 @@ export default function NYSubway() {
         <>
             <p>
                 <b>The cleaner your product is the more your customer will maintain it like that.</b><br/><br/>
+                
                 <img className="m-auto filter-grayscale max-h-80" src="nysubway.png" alt="nysubway"/> <br/><br/>
 
-                I would love to remember from which book I read the next statement: <br/>
+                I would love to remember from which book I read a brief story about the NY's Subway trains that goes something like this:: <br/>
+                <i>
                 There was a time (near 1980) where New York was having struggle to maintain their subway trains clean and good looking.<br/>
                 They noticed that when the train is clean and maintained people treat it with more care. Making it easier to maintain.<br/>
                 And if the train were dirty and broken, people will treat it with less care. <br/>
+                </i>
                 In software happens the same. <br/>
                 If your product is broken, looks bad and doesn't work as intended. <br/>
                 Your customer will probably not report issues as much as if your product were in a good shape. <br/>
