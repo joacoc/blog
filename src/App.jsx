401d253f8386cf4d28a82b64eaf3ddd983b33e5b
Joaquin Colacci <joaquincolacci@gmail.com>
2021-06-30 22:40:36 -0300

    ETH

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,76 @@
 import React from 'react';
-import './styles.css';
-export default App;
+import Eth from 'web3-eth';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [eth, setEth] = useState(undefined);
+  const [err, setErr] = useState(undefined);
+  const [accounts, setAccounts] = useState([]);
+  const [balances, setBalances] = useState(new Map());
+
+  useEffect(() => {
+    const ethClient = new Eth(Eth.givenProvider);
+    setEth(ethClient);
+  }, []);
+
+  useEffect(() => {
+    if (eth) {
+      eth.getAccounts().then((ethAccounts) => {
+        if (ethAccounts.length > 0) {
+          setAccounts(ethAccounts);
+        } else {
+          const { ethereum } = window;
+          if (ethereum) {
+            ethereum.enable();
+            ethereum.on('accountsChanged', function (accounts) {
+              setAccounts(accounts);
+            });
+          }
+        }
+      }).catch((errAccounts) => {
+        console.log(errAccounts);
+        setErr('Error getting accounts.');
+      })
+    }
+  }, [eth]);
+
+  useEffect(() => {
+    if (accounts.length > 0) {
+      const balancesPromises = [];
+      accounts.forEach((account) => {
+        balancesPromises.push(eth.getBalance(account))
+      });
+      Promise.all(balancesPromises).then((allBalances) => {
+        const newBalances = new Map();
+        accounts.forEach((account, index) => {
+          newBalances.set(account, allBalances[index]);
+        });
+
+        setBalances(newBalances);
+      }).catch((errBalance) => {
+        console.error(errBalance);
+        setErr("Error getting balances.");
+      });
+    }
+  }, [eth, accounts])
+
   return (<>
-    <body>
-      <h1 style={{ color: "green" }}>
+    <div style={{ backgroundColor: "black", height: "100%" }}>
+      <h1 style={{ color: "green", textAlign: "center" }}>
         CENTRO DE INTELIGENCIA DE AXIE PEPOSO
       </h1>
-      <div style={{ display: "flex", flex: "row", marginLeft: "5%", marginRight: "5%" }}>
+      <div style={{ color: "white", marginLeft: "2rem" }}>
+        {err
+          ? <p>Error: {err}</p>
+          : (
+            <div style={{ display: "flex", flexFlow: "column" }}>
+              <h3>Accounts: </h3>
+              <p style={{}}>{accounts.map(account => <p>{account} - Balance: ${balances.get(account)}</p>)}</p>
+            </div>
+          )}
+      </div>
+      <div style={{ display: "flex", flexWrap: "column", marginLeft: "5%", marginRight: "5%" }}>
         <div style={{ borderRadius: 20, textAlign: "center", color: "turquoise" }}>
           <h3>Precio: Small love potion</h3>
           <iframe
@@ -54,6 +116,8 @@ function App() {
         />
       </div>
 
-    </body>
+    </div>
   </>)
 }
+
+export default App;
